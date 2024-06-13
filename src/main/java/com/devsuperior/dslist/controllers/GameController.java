8017b63f1e1a395f59e8a2bb56ce677bb050f179
Nicolas <nicolasbarsain2017@gmail.com>
2024-06-13 13:33:40 -0300

    Initial

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import com.devsuperior.dslist.services.GameService;
 
 @RestController
 //fazer o mapeamento do recurso
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "/games") //configuro o caminho q vai ser respondido na api
 public class GameController { //ele vai ser a porta de entrada do backend, ele quem disponibiliza a API
 	
